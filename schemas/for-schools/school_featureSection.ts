eb1a16fc04fe95fd66b874d6b19a8102239c6cd9
Tharuk Methdinu <hopelearning.edu@gmail.com>
2025-11-02 19:33:44 +0530

    Add school_featureSection schema for managing feature blocks

---
+++ schemas/for-schools/school_featureSection.ts
@@ -0,0 +1,61 @@
+// schemas/featureSection.ts
+export default {
+  name: 'school_featureSection',
+  title: 'School Feature Sections',
+  type: 'document',
+  fields: [
+    {
+      name: 'features',
+      title: 'Feature Blocks',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          title: 'Feature',
+          fields: [
+            { 
+              name: 'title', 
+              type: 'string', 
+              title: 'Title' 
+            },
+            {
+              name: 'description',
+              title: 'Description with Bullet Points',
+              type: 'array',
+              of: [
+                {
+                  type: 'block',
+                  styles: [{ title: 'Normal', value: 'normal' }],
+                  lists: [{ title: 'Bullet', value: 'bullet' }],
+                  marks: {
+                    decorators: [
+                      { title: 'Strong', value: 'strong' },
+                      { title: 'Emphasis', value: 'em' },
+                    ],
+                    annotations: [],
+                  },
+                }
+              ]
+            },
+            { 
+              name: 'buttonText', 
+              type: 'string', 
+              title: 'Button Text' 
+            },
+            { 
+              name: 'isDisabled', 
+              type: 'boolean', 
+              title: 'Disable Button?' 
+            },
+            {
+              name: 'image',
+              type: 'image',
+              title: 'Image',
+              options: { hotspot: true },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
